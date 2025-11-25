7f4cb11ccbdc66cf16410dd782ea135377d22823
baba <it@solution-plus.es>
2025-11-25 14:16:28 +0000

    deployment config

--- dist/libs/pipes/validateId.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@nestjs/common';
-export declare class ValidateUUIDPipe implements PipeTransform<string> {
-    transform(value: string): string;
-    private isValidUUID;
-}
